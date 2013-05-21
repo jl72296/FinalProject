b29fe2ad2eb308e332ead7151f9fbfc19d9be65e
jl72296 <jorlop531@gmail.com>
2013-05-20 19:26:24 -0600

    Create Monkey.java

---
+++ Monkey.java
@@ -0,0 +1,41 @@
+
+public class Monkey extends Bi_ped {
+  public void Monkey()
+	{
+		super.setDialog(2);
+		super.setAnimalSpeed(3);
+	}
+	
+	public String Talk()
+	{
+		String animalName = "";
+		String id = "";
+		int nameSelector;
+		nameSelector = 1 + (int)(Math.random()*5);
+		switch ( nameSelector )
+		{
+		case 1:
+			animalName = "Lorena";
+		case 2:
+			animalName = "Tamir";
+		case 3:
+			animalName = "Bubbles";
+		case 4:
+			animalName = "Koko";
+		default:
+			animalName = "George";
+			break;
+		}
+		id = animalName + ", the monkey,";
+		return id + super.Talk();
+	}
+	
+	public String Walk()
+	{
+		String msg;
+		msg = "My animal is a Monkey ";
+		msg = msg + super.Walk();
+		return msg;
+	}
+
+}

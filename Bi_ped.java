fe56eb237c6251884e7088998582d8c4577be8f0
jl72296 <jorlop531@gmail.com>
2013-05-20 19:26:05 -0600

    Create Bi_ped.java

---
+++ Bi_ped.java
@@ -0,0 +1,21 @@
+
+public class Bi_ped extends Animal{
+
+  public String Talk()
+	{
+		String msg;
+		msg = " a Bi-Ped Animal says, ";
+		msg = msg + super.Talk();
+		return msg;
+	}
+	
+	public String Walk()
+		{
+			String msg;
+			msg = "which is a Bi-Ped ";
+			msg = msg + super.Walk();
+			return msg;
+		}
+
+
+}
